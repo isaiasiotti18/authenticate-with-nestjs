2229efc68e1fa2c8b7a74b5079e802961832484b
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-03-23 18:04:12 -0300

    feat(auth): implement login, local auth and guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [UserModule, PassportModule, JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
