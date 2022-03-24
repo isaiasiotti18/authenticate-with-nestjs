7965d4ef322845f2280d40d496482f84e985e411
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-03-24 16:47:47 -0300

    feat(auth): implement jwt validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local-strategy';
 
 @Module({
@@ -16,6 +17,6 @@ import { LocalStrategy } from './strategies/local-strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
